7a7677331f4e53a11899419d5b25b970f7105a00
camilomachuca <camilomachuca03@gmail.com>
2024-04-12 13:14:13 -0500

    Ejercicio1

---
+++ Dia4/Ejercicio1_Machuca.py
@@ -0,0 +1,49 @@
+#--------------------------------
+#-------- DIA 4 PYTHON--------
+#--------------------------------
+#Se le da la vienvenida al usuario 
+print("Bienvenido usuario")
+Denominacion= [1]
+cont=0
+Denomin=[5]
+
+Denomina=[10]
+contadorDenomi=0
+cambio=[]
+Dinero=int(input("ingrese la cantidad de dinero que quiere cambiar"))
+contadorr=10
+
+
+
+while contadorr<=Dinero:
+    
+    if contadorr<= Dinero:
+        contadorr=contadorr+10
+        conta=contadorr
+        
+        
+    if conta<= Dinero:
+      
+      conta=conta+5
+      co=conta
+
+
+    if co<=Dinero:
+            co=co+1
+            contad=co
+
+    print("El cambio que se le da al usuario es")
+             
+            
+    print(contadorr)
+                    
+            
+    print (conta)
+
+            
+    print(co)
+
+    True
+        
+
+  #Desarrollado por Camilo Machuca Vega TI 1090397640
