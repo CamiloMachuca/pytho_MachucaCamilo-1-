ad5f94e24c7a0f128b208127a8163a83e15e2650
camilomachuca <camilomachuca03@gmail.com>
2024-04-16 14:02:02 -0500

    Hola

---
+++ Dia6/Ejercicio1_MachucaCamilo.py
@@ -0,0 +1,11 @@
+#--------------------------------
+#-------- DIA 6 PYTHON--------
+#--------------------------------
+#Programa para eliminar el numero repetido de una lista
+lista=input()
+
+listaNueva=sorted(list(lista))
+listaSinRepetir=(list(set(listaNueva)))
+print(listaSinRepetir)
+
+#Desarrollado por Camilo Machuca Vega TI 1090397640
---
+++ Dia6/Ejercicio2_MachucaCamilo.py
@@ -0,0 +1,7 @@
+Numeros=input()
+n=input()
+Numeros=list(Numeros)
+
+
+for i in range(Numeros):
+    nu= (Numeros+n)
