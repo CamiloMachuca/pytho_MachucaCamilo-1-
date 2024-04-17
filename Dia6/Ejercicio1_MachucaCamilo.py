31e3ced4b16d63e2b5a2a540e4fb8373a83ff00c
camilomachuca <camilomachuca03@gmail.com>
2024-04-16 22:35:36 -0500

    Hola

--- Dia6/Ejercicio1_MachucaCamilo.py
+++ Dia6/Ejercicio1_MachucaCamilo.py
@@ -2,10 +2,12 @@
 #-------- DIA 6 PYTHON--------
 #--------------------------------
 #Programa para eliminar el numero repetido de una lista
-lista=input()
+lista=input()# se guarda la lista dada por el usuario en una variable llamada lista
 
-listaNueva=sorted(list(lista))
-listaSinRepetir=(list(set(listaNueva)))
-print(listaSinRepetir)
+for i in range(100):
+    listaSinRepetir=(sorted(set(lista)))# se organiza la lista de menor a mayor y se eliminan los numeros repetidos
+
+print(listaSinRepetir)#se le muestra al usuario
+
 
 #Desarrollado por Camilo Machuca Vega TI 1090397640
--- Dia6/Ejercicio2_MachucaCamilo.py
+++ Dia6/Ejercicio2_MachucaCamilo.py
@@ -1,7 +1,67 @@
-Numeros=input()
-n=input()
-Numeros=list(Numeros)
+#--------------------------------
+#-------- DIA 6 PYTHON--------
+#--------------------------------
+def funcion_numeros(numeros, target): #se crea la funcion para encontrar los dos numeros 
+    lugar=[]
+    contador=(0)
+    for i in range(len(numeros) - 1):# bucle para buscar los dos numeros que sumados me den igual a returt
+        
+        
+
+        for j in range(i + 1, len(numeros)): # se verifica si la suma de numeros[i] y numeros[j] es igual a returt
+            
+            
+            if numeros[i] + numeros[j] == target:# Retornar los dos números que al sumarlos son iguales a returt
+                
+               
+                return (numeros[i], numeros[j]) # Si no se encuentran números que al sumarlos den igual a target se reinicia el siclo hasta encontrarlo
+contador=(0)
 
 
-for i in range(Numeros):
-    nu= (Numeros+n)
+
+def main(): # se define a main para que se ejecute en primera instancia ya que de alli sale los valores para la funcion
+    
+    lista = input()# se le pide la lista de numeros al usuario
+    
+    numeros = list(map(int,lista.strip().split(","))) #se pasan los numeros dados por el usuario a una lista y se separan las cadenas en donde estan las comas con split
+
+    target = int(input())#se le pide al usuario el numero que quiere que encuentre la pareja a sumar y que sea igual a target
+
+    
+    suma = funcion_numeros(numeros, target)# se busca los dos números que sumen igual a starget
+
+    if suma:# se muestra el resultado
+        print(f" {suma[0]}, {suma[1]}")
+        
+   
+
+
+if __name__ == "__main__":# se ejecuta la función main si este es el módulo principal a realizarse
+    main()
+
+
+#Desarrollado por Camilo Machuca Vega TI 1090397640
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
